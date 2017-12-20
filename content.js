f4f38337f90fec3cf5010fe34c997c165ae172a0
ChaseKiner <chasekiner@hotmail.com>
2017-12-20 00:03:42 +0000

    Add selectWord function

--- content.js
+++ content.js
@@ -1,3 +1,12 @@
+window.addEventListener('mouseup', selectWord);
 
-document.getElementById("sText").innerHTML = text;
-text = window.getSelection().toString();
+function selectWord() {
+  let selectedText = window.getSelection().toString().trim();
+
+  if (selectedText.length > 0) {
+    let message = {
+      text: selectedText
+    };
+    chrome.runtime.sendMessage(message);
+  }
+}
